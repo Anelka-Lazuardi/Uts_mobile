e9989e9a72fa0f36692c53ead6d71041f81103b3
Anelka-Lazuardi <alanelka42@gmail.com>
2020-11-30 19:42:05 +0700

    pull from master

--- lib/views/ui3.dart
+++ lib/views/ui3.dart
@@ -7,55 +7,88 @@ class Ui3 extends StatelessWidget {
       appBar: AppBar(
         title: Text('Konsultasi'),
       ),
-
-      body: Column(children: [
-        Flexible(
-          flex: 8,
-          child: ListView(
-            children: [
-              ListTile(
-                title: Text("Tanya KRS"),
-                // leading: Icon(Icons.forum, size: 20.0,),
-                // trailing: Text("4"),
-              ),
-
-              Divider(
-                color: Colors.black,
-              ),
-
-              ListTile(
-                title: Text("Pak mohon maaf, untuk semester ini, sebaiknya mata kuliah apa saja yang saya ambil."),
-              ),
-
-              ListTile(
-                title: Text("Terimakasih sebelumnya"),
-              ),
-
-              ListTile(
-                title: Text("Sebaiknya Pilih Matakuliah:"
-                    "- Pemrograman Mobile"
-                    "- Kecerdasan Buatan"),
-              ),
-
-            ],
+      body:
+          Column(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+        Container(
+          child: Flexible(
+            child: ListView(
+              children: [
+                ListTile(
+                  title: Text("Tanya KRS"),
+                  // leading: Icon(Icons.forum, size: 20.0,),
+                  // trailing: Text("4"),
+                ),
+                Divider(
+                  color: Colors.black,
+                ),
+                ListTile(
+                  title: Text(
+                      "Pak mohon maaf, untuk semester ini, sebaiknya mata kuliah apa saja yang saya ambil."),
+                ),
+                ListTile(
+                  title: Text("Terimakasih sebelumnya"),
+                ),
+                // ListTile(
+                //   title: Text("Sebaiknya Pilih Matakuliah:"
+                //       "- Pemrograman Mobile"
+                //       "- Kecerdasan Buatan"),
+                // ),
+                Container(
+                  color: Color(0xFFB8DEFF),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Sebaiknya Pilih Matakuliah:",
+                        ),
+                        Text("- Pemrograman Mobile"),
+                        Text("- Kecerdasan Buatan"),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
-
-        // Flexible(
-        //   flex: 1,
-        //   child: SizedBox.expand(
-        //     child: Padding(
-        //       padding: const EdgeInsets.fromLTRB(8, 8, 8, 17),
-        //       child: RaisedButton(
-        //         onPressed: () {},
-        //         child: Icon(Icons.message),
-        //         textColor: Colors.white,
-        //         color: Color(0xFFfd8c04),
-        //       ),
-        //     ),
-        //   ),
-        // ),
-
+        Container(
+          child: Flexible(
+            child: Row(
+              children: <Widget>[
+                new Flexible(
+                  flex: 4,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextField(
+                      keyboardType: TextInputType.multiline,
+                      maxLines: 8,
+                      decoration: new InputDecoration(
+                        focusedBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.black, width: 1.0),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.black, width: 1.0),
+                        ),
+                        // hintText: 'Mobile Number',
+                      ),
+                    ),
+                  ),
+                ),
+                new Flexible(
+                  flex: 1,
+                  child: FlatButton(
+                    child: Icon(Icons.send),
+                    onPressed: () {},
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
       ]),
     );
   }
